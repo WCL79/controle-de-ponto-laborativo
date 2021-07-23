34775516ead84cf137bda92d6a67fc502e1588f8
weslley lima <weslleyclima@yahoo.com.br>
2021-07-23 11:42:01 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/wcl/controledeponto/ControleDePontoLaboralApplication.java
@@ -0,0 +1,13 @@
+package br.com.wcl.controledeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePontoLaboralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePontoLaboralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wcl/controledeponto/ControleDePontoLaboralApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wcl.controledeponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoLaboralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
